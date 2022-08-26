171579c8bd6dd03ac8728cbf6cde212e0277a0fd
Del Campo Fabian <fabydelcampo@hotmail.com>
2022-08-26 04:50:21 -0300

    Desafio Navbar ReactJS

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import '../App.css';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand mx-2 md-mx-5" href="#">Camping</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse d-flex-md justify-content-around" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item sm-mx-2 mx-5">
+                            <a className="nav-link active" aria-current="page" href="#">Productos</a>
+                        </li>
+                        <li className="nav-item sm-mx-2 mx-5">
+                            <a className="nav-link active" href="#"><i className="fa-solid fa-cart-shopping"></i></a>
+                        </li>
+                        <li className="nav-item sm-mx-2 mx-5">
+                            <a className="nav-link active" href="#">Nosotros</a>
+                        </li>
+                        <li className="nav-item sm-mx-2 mx-5">
+                            <a className="nav-link active">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
